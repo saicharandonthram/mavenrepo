b612bd07a3d0a5b2a9507ca899be8f029372578f
saicharandonthram <72463955+saicharandonthram@users.noreply.github.com>
2020-10-30 14:37:24 +0530

    first commit
    uploading all files

---
+++ samplemavenproject/src/main/java/org/temp/com/samplemavenproject/App.java
@@ -0,0 +1,13 @@
+package org.temp.com.samplemavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
